732cb0f0a25cfa373d3e475a11d4158ddbc59494
Max <weebgor7@mail.ru>
2019-10-17 21:32:30 +0300

    added footer and header apaptibe

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,43 @@
 $(function(){
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 800,
+        grid: true,
+        prefix: "$"
+        });
+    
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+  
     var mixer = mixitup('.products__inner-box');
 
 
-    $(".rate-star").rateYo({
-        rating: 5,
-        starWidth: "12px",
-        readOnly: true
-      });
-
+   
       $('.product-slider__inner').slick({
         dots: true,
         arrows: false,
@@ -16,4 +45,24 @@ $(function(){
         slidesToScroll: 4,
       });
 
-});
+      
+}); 
+ $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+ $('.icon-th-list').on('click', function(){
+     $('.products__item').addClass('list');
+     $('.icon-th-list').addClass('active');
+     $('.icon-th-large').removeClass('active');
+
+ });
+ $('.icon-th-large').on('click', function(){
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+ });
+ 
+    
